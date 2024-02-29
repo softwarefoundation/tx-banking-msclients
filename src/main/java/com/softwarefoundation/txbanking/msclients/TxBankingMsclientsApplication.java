cc267f7ba8502923406d52e508047c3d68953e0b
Dherkyan R. Silva <dherkyan.silva.prestador@prf.gov.br>
2024-02-29 09:14:01 -0300

    Início projeto tx-banking-msclients

---
+++ src/main/java/com/softwarefoundation/txbanking/msclients/TxBankingMsclientsApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.txbanking.msclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxBankingMsclientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxBankingMsclientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarefoundation/txbanking/msclients/TxBankingMsclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.txbanking.msclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxBankingMsclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
